8b15a84be8b72dcfba8b43feeca08b8bf0f94f83
jgaste <julien.gaste@artelys.com>
2025-07-18 19:18:03 +0200

    :star: : update env vars for deployment

--- backend/conversations_api/config.py
+++ backend/conversations_api/config.py
@@ -1,4 +1,3 @@
 import os
 
-MONGO_DB_NAME = os.environ.get("MONGO_DB_NAME", "bourracho_api_db2")
-MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "mongodb://localhost:27017")
+MONGO_DB_NAME = os.environ.get("MONGO_DB_NAME", "bourracho_api_test")
